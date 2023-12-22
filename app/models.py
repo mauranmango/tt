6633b1100ea8a57bf08cbb81aebc6d209f11539a
Mauran Mango <mauran.mango@yahoo.com>
2023-12-22 17:02:44 +0100

    new model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,28 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
     # query: db.Query
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
-    id = db.Column(db.Integer, primari_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"User: <{self.username}>"
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+
+    def __repr__(self):
+        return f"Post: <{self.body}>"
--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret_key'
     API_KEY = os.environ.get('API_KEY') or 'donotsharewithanyone'
     DEBUG = os.environ.get('DEBUG') or True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:8880@localhost:5432/tinyblog"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:8887@localhost:5432/tinyblog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False

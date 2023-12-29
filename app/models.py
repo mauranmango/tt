b64564d8998f3500fdbd764efd83355975a02770
Mauran Mango <mauran.mango@yahoo.com>
2023-12-29 15:49:51 +0100

    modified login function and protected other view functions with login_required dicorator

--- app/models.py
+++ app/models.py
@@ -1,22 +1,36 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
-    # query: db.Query
-    __tablename__ = "user"
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    query: db.Query
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String, index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"User: <{self.username}>"
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
+    query: db.Query
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-import inspect
 from app import app
 from flask import render_template, request, session, redirect, flash, url_for
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
+from app.models import User, Post
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': "ben"}
     posts = [
@@ -23,20 +26,31 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user: {form.username.data} -- remember_me:{form.remember_me.data}")
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, form.remember_me.data)
+        flash(f"User logged in: {form.username.data} -- remember_me:{form.remember_me.data}")
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title="Sign In", form=form)
 
 
-@app.route('/api')
-def show_api():
-    return f'API_KEY= {app.config.get("API_KEY")}, ' \
-           f'SECRET_KEY= {app.config.get("SECRET_KEY")},'
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/thank_you')
+@login_required
 def thank_you():
-    # text = dict(request.headers)  # save all headers as a dictionary
     return render_template('thank_you.html', title='Thank You')
